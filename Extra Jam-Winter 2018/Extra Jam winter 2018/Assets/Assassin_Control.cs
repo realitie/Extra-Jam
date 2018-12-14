2d28d04b097e7a2875952b7ed389abbd94de5c88
Realitie <leagueshark@gmail.com>
2018-12-14 16:28:43 -0500

    Day 1

---
+++ Extra Jam-Winter 2018/Extra Jam winter 2018/Assets/Assassin_Control.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Assassin_Control : MonoBehaviour {
+    // Use this for initialization
+    private float speed = 3f;
+    void Start () {
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (Input.GetKey(KeyCode.A) & Input.GetKey(KeyCode.W))
+            transform.Translate((Vector3.right + Vector3.back) * speed * 0.707f * Time.fixedDeltaTime);
+        else if (Input.GetKey(KeyCode.W) & Input.GetKey(KeyCode.D))
+            transform.Translate((Vector3.left + Vector3.back) * speed * 0.707f * Time.fixedDeltaTime);
+        else if (Input.GetKey(KeyCode.D) & Input.GetKey(KeyCode.S))
+            transform.Translate((Vector3.forward + Vector3.left) * speed * 0.707f * Time.fixedDeltaTime);
+        else if (Input.GetKey(KeyCode.S) & Input.GetKey(KeyCode.A))
+            transform.Translate((Vector3.forward + Vector3.right) * speed * 0.707f * Time.fixedDeltaTime);
+        else if (Input.GetKey(KeyCode.A))
+            transform.Translate(Vector3.right * speed * Time.fixedDeltaTime);
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.left * speed * Time.fixedDeltaTime);
+            transform.Translate(Vector3.up * 0.01f * Time.fixedDeltaTime);
+        }
+        else if (Input.GetKey(KeyCode.W))
+            transform.Translate(Vector3.back * speed * Time.fixedDeltaTime);
+        else if (Input.GetKey(KeyCode.S))
+            transform.Translate(Vector3.forward * speed * Time.fixedDeltaTime);
+    }
+}
---
+++ Extra Jam-Winter 2018/Extra Jam winter 2018/Assets/Reticle_Follow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reticle_Follow : MonoBehaviour {
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hit = new RaycastHit();
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            transform.position = Vector3.Lerp(
+                new Vector3(transform.position.x, 10, transform.position.z),
+                new Vector3(hit.point.x, 10, hit.point.z),
+                0.04f);
+        }
+        
+
+        
+    }
+}
